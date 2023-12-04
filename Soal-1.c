ec0c28b5beb4a3ff57bc28b341e897c607c47208
4lannc <alannc856@gmail.com>
2023-12-04 10:47:46 +0700

    uts alan

---
+++ Soal-1.c
@@ -0,0 +1,18 @@
+//MUHAMMAD HAFID MAULANA
+//202311009
+
+#include <stdio.h>
+
+int main (){
+    int panjang, lebar;
+    int luas;
+    
+    printf("Masukkan panjang : ");
+    scanf("%d", &panjang);
+    printf("Masukkan lebar : ");
+    scanf("%d", &lebar);
+    luas = panjang * lebar;
+    printf("Hasilnya : %d", luas);
+
+return 0;
+}
